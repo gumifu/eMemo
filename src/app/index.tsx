8a9ddd03860f83ae64238df799186f1e48edad66
藤﨑文崇 <ffujisaki@fujitakashinoMacBook-Pro.local>
2024-01-03 21:53:15 +0900

    eMemo start

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,61 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
   return (
+    <View style={styles.container}>
+      <View>
         <View>
-            <Text>Hello World</Text>
+          <Text>eMemo</Text>
+          <Text>ログアウト</Text>
         </View>
+      </View>
+      <View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年01月03日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年01月03日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年01月03日 10:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>＋</Text>
+      </View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default Index
